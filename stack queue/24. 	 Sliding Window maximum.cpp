da2d6944ba295d5535320928245cbfbff35e2815
diyasharma12 <145861099+diyasharma12@users.noreply.github.com>
2024-06-23 15:12:52 +0530

    Create 24. 	 Sliding Window maximum.cpp

---
+++ stack queue/24. 	 Sliding Window maximum.cpp
@@ -0,0 +1,30 @@
+//use 2 loops 1 for n and k
+
+//iterate 
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+         
+        deque<int> q;
+        int max1 = INT_MIN;
+
+        vector<int> store;
+
+        for(int i = 0 ;i < nums.size(); i++)
+        {
+            if(!q.empty() && q.front() == i - k)
+            {
+                q.pop_front();
+            }
+            while(!q.empty() && nums[q.back()] <= nums[i])
+            {
+                q.pop_back();
+            }
+            q.push_back(i);
+            if(i >= k - 1){
+                store.push_back(nums[q.front()]);
+            }
+        }
+        return store;
+    }
+};
